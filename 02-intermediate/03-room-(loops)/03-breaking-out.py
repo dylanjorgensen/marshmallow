9d1b56619a217c40975f6935b192e84aeb310c7f
dylanjorgensen <dylan@dylanjorgensen.com>
2016-10-22 13:30:47 -0700

    Variable updates

--- 02-intermediate/03-room-(loops)/03-breaking-out.py
+++
@@ -1,8 +0,0 @@
-while guesses_left > 0:
-    guess = int(raw_input("What is your guess"))
-    if guess == random_number:
-        print 'You win!'
-        break
-    guesses_left -= 1
-else:
-    print "You lose"
--- 02-intermediate/03-room-(loops)/04-nested.py
+++
@@ -1,9 +0,0 @@
-
-
-
-num_list = [[1,2,3],[10,20,30],[100,200,330]]
-
-
-for x in range(0,3):
-    for y in range(0,3):
-        print(num_list [x] [y])
